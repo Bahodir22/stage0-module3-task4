9287d268636af4f459559a975ec55d4b93414c5d
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-09-20 11:50:54 +0500

    1
    ...

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int letterC=99, letterH=104, letterA=97, letterR=114;
+        char c=(char)letterC, h=(char)letterH, a=(char)letterA, r=(char)letterR;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
